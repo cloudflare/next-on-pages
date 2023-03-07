72fcd0882076f642098699df8b7a379d7bd35460
James <james@eli.cx>
2023-03-07 14:31:14 +0000

    fix: windows file paths not matching middleware manifest

--- src/index.ts
+++ src/index.ts
@@ -305,9 +305,10 @@ const transform = async ({
           await mkdir(dirname(newFilePath), { recursive: true });
           await writeFile(newFilePath, contents);
 
+          // `\\` needs to be replaced with `\` for Windows so that it can match the path correctly in the middleware manifest.
           functionsMap.set(
-            relative(functionsDir, filepath).slice(0, -".func".length),
-            newFilePath
+            relative(functionsDir, filepath).slice(0, -".func".length).replace(/\\/g, "/"),
+            newFilePath.replace(/\\/g, "/")
           );
         } else if (isDirectory) {
           await walk(filepath);
