8a14ee3cf655a5769d9dce2034a9c90e88b1df1a
Dario Piotrowicz <dario@cloudflare.com>
2023-03-15 11:57:10 +0000

    adjust request so that it contains geo headers
    resolves #46

--- templates/_worker.js/index.ts
+++ templates/_worker.js/index.ts
@@ -158,10 +158,29 @@ export default {
       }
 
       if (found) {
-        return entrypoint.default(request, context);
+        const adjustedRequest = adjustRequestForVercel(request);
+        return entrypoint.default(adjustedRequest, context);
       }
     }
 
     return env.ASSETS.fetch(request);
   },
 } as ExportedHandler<{ ASSETS: Fetcher }>;
+
+/**
+ * Adjusts the request so that it is formatted as if it were provided by Vercel
+ *
+ * @param request the original request received by the worker
+ * @returns the adjusted request to pass to Next
+ */
+function adjustRequestForVercel(request: Request): Request {
+  const adjustedHeaders = new Headers(request.headers);
+
+  adjustedHeaders.append("x-vercel-ip-city", request.cf?.city);
+  adjustedHeaders.append("x-vercel-ip-country", request.cf?.country);
+  adjustedHeaders.append("x-vercel-ip-country-region", request.cf?.region);
+  adjustedHeaders.append("x-vercel-ip-latitude", request.cf?.latitude);
+  adjustedHeaders.append("x-vercel-ip-longitude", request.cf?.longitude);
+
+  return new Request(request, { headers: adjustedHeaders });
+}
