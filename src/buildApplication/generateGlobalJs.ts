8b4f6d5e52640b5dcb7d2e36bf0109f25cdf708a
Dario Piotrowicz <dario@cloudflare.com>
2023-04-03 13:25:27 +0100

    allow setting of NODE_ENV (#141)
    * allow setting of NODE_ENV
    
    resolves #4

---
+++ src/buildApplication/generateGlobalJs.ts
@@ -0,0 +1,42 @@
+import { cliWarn } from '../cli';
+
+/**
+ * Generates the javascript content (as a plain string) that deals with the global scope that needs to be
+ * added to the built worker.
+ *
+ * @returns the plain javascript string that should be added at the top of the the _worker.js file
+ */
+export function generateGlobalJs(): string {
+	const nodeEnv = getNodeEnv();
+	return `globalThis.process = { env: { NODE_ENV: '${nodeEnv}' } };`;
+}
+
+enum NextJsNodeEnv {
+	PRODUCTION = 'production',
+	DEVELOPMENT = 'development',
+	TEST = 'test',
+}
+
+function getNodeEnv(): string {
+	const processNodeEnv = process.env.NODE_ENV;
+
+	if (!processNodeEnv) {
+		return NextJsNodeEnv.PRODUCTION;
+	}
+
+	const nextJsNodeEnvs = Object.values(NextJsNodeEnv);
+	if (!(nextJsNodeEnvs as string[]).includes(processNodeEnv)) {
+		cliWarn('');
+		cliWarn(`
+			WARNING:
+			    The current value of the environment variable NODE_ENV is "${processNodeEnv}",
+			    but the supported values are: ${nextJsNodeEnvs
+						.map(env => `"${env}"`)
+						.join(', ')}.
+			    See: https://nextjs.org/docs/basic-features/environment-variables.
+		`);
+		cliWarn('');
+	}
+
+	return processNodeEnv;
+}
--- templates/_worker.js/globals.js
+++
@@ -1,1 +0,0 @@
-globalThis.process = { env: { NODE_ENV: 'production' } };
