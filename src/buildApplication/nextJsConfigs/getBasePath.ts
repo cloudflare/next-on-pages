a75190e5adb3e40888b707314cb43309f85503e4
Dario Piotrowicz <dario@cloudflare.com>
2023-04-17 21:21:38 +0100

    fix filefalling typo

--- src/buildApplication/nextJsConfigs/getBasePath.ts
+++ src/buildApplication/nextJsConfigs/getBasePath.ts
@@ -27,8 +27,7 @@ export async function getBasePath(): Promise<string | null> {
 	if (!routesManifest || routesManifest.version !== 3) {
 		cliWarn(
 			`
-			  Warning: Could not read basePath from .next/routes-manifest.json 
-				filefalling back to empty basePath
+			Warning: Could not read basePath from .next/routes-manifest.json file, falling back to empty basePath
 			`,
 			{ spaced: true }
 		);
