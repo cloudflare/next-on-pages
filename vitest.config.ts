b8b0bdedec9f439679aa76dea4b4085e48518df4
James <james@eli.cx>
2023-04-17 10:01:33 +0100

    fix: use miniflare for vitest due to edge runtime environment bug (#179)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		include: ['tests/**/*.test.ts'],
-		environment: 'edge-runtime',
+		environment: 'miniflare',
 	},
 });
