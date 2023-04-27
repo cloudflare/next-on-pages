1e742d95341fa97fccbd6a962094d3f24cd3332f
Dario Piotrowicz <dario@cloudflare.com>
2023-04-27 13:26:35 +0100

    add os to getCurrentPackageManager tests

--- tests/src/buildApplication/getCurrentPackageManager.test.ts
+++ tests/src/buildApplication/getCurrentPackageManager.test.ts
@@ -8,25 +8,25 @@ import type { PackageManager } from '../../../src/utils';
 
 describe('getCurrentPackageManager', async () => {
 	it('should detect yarn (berry)', async () => {
-		await testWith('yarn (berry)', async () => {
+		await testWith({ packageManager: 'yarn (berry)' }, async () => {
 			const pkgMng = await getCurrentPackageManager();
 			expect(pkgMng).toEqual('yarn (berry)');
 		});
 	});
 	it('should detect yarn (classic)', async () => {
-		await testWith('yarn (classic)', async () => {
+		await testWith({ packageManager: 'yarn (classic)' }, async () => {
 			const pkgMng = await getCurrentPackageManager();
 			expect(pkgMng).toEqual('yarn (classic)');
 		});
 	});
 	it('should detected pnpm', async () => {
-		await testWith('pnpm', async () => {
+		await testWith({ packageManager: 'pnpm' }, async () => {
 			const pkgMng = await getCurrentPackageManager();
 			expect(pkgMng).toEqual('pnpm');
 		});
 	});
-	it('should detected pnpm', async () => {
-		await testWith('npm', async () => {
+	it('should detected npm', async () => {
+		await testWith({ packageManager: 'npm' }, async () => {
 			const pkgMng = await getCurrentPackageManager();
 			expect(pkgMng).toEqual('npm');
 		});
@@ -35,43 +35,89 @@ describe('getCurrentPackageManager', async () => {
 
 describe('getCurrentPackageExecuter', () => {
 	it('should detect yarn (berry)', async () => {
-		await testWith('yarn (berry)', async () => {
+		await testWith({ packageManager: 'yarn (berry)' }, async () => {
 			const pkgMng = await getCurrentPackageExecuter();
 			expect(pkgMng).toEqual('yarn dlx');
 		});
 	});
 	it('should detect yarn (classic)', async () => {
-		await testWith('yarn (classic)', async () => {
+		await testWith({ packageManager: 'yarn (classic)' }, async () => {
 			const pkgMng = await getCurrentPackageExecuter();
 			expect(pkgMng).toEqual('yarn');
 		});
 	});
 	it('should detected pnpm', async () => {
-		await testWith('pnpm', async () => {
+		await testWith({ packageManager: 'pnpm' }, async () => {
 			const pkgMng = await getCurrentPackageExecuter();
 			expect(pkgMng).toEqual('pnpx');
 		});
 	});
-	it('should detected pnpm', async () => {
-		await testWith('npm', async () => {
+	it('should detected npm', async () => {
+		await testWith({ packageManager: 'npm' }, async () => {
 			const pkgMng = await getCurrentPackageExecuter();
 			expect(pkgMng).toEqual('npx');
+		});
+	});
+
+	it('should detect yarn (berry) on windows', async () => {
+		await testWith(
+			{ packageManager: 'yarn (berry)', os: 'windows' },
+			async () => {
+				const pkgMng = await getCurrentPackageExecuter();
+				expect(pkgMng).toEqual('yarn.cmd dlx');
+			}
+		);
+	});
+	it('should detect yarn (classic) on windows', async () => {
+		await testWith(
+			{ packageManager: 'yarn (classic)', os: 'windows' },
+			async () => {
+				const pkgMng = await getCurrentPackageExecuter();
+				expect(pkgMng).toEqual('yarn.cmd');
+			}
+		);
+	});
+	it('should detected pnpm on windows', async () => {
+		await testWith({ packageManager: 'pnpm', os: 'windows' }, async () => {
+			const pkgMng = await getCurrentPackageExecuter();
+			expect(pkgMng).toEqual('pnpx.cmd');
+		});
+	});
+	it('should detected npm on windows', async () => {
+		await testWith({ packageManager: 'npm', os: 'windows' }, async () => {
+			const pkgMng = await getCurrentPackageExecuter();
+			expect(pkgMng).toEqual('npx.cmd');
 		});
 	});
 });
 
 async function testWith(
-	pkgMng: Exclude<PackageManager, 'yarn'>,
+	{
+		packageManager,
+		os = 'linux/macos',
+	}: {
+		packageManager: Exclude<PackageManager, 'yarn'>;
+		os?: 'windows' | 'linux/macos';
+	},
 	test: () => Promise<void>
 ): Promise<void> {
-	currentPackageManagerMock = pkgMng;
-	vi.stubEnv('npm_config_user_agent', pkgMng);
+	currentMocks.packageManager = packageManager;
+	currentMocks.os = os;
+	vi.stubEnv('npm_config_user_agent', packageManager);
+	vi.spyOn(process, 'platform', 'get').mockReturnValue(
+		currentMocks.os === 'windows' ? 'win32' : 'linux'
+	);
 	await test();
 	vi.unstubAllEnvs();
 }
 
-let currentPackageManagerMock: Exclude<PackageManager, 'yarn'> =
-	'yarn (classic)';
+const currentMocks: {
+	packageManager: Exclude<PackageManager, 'yarn'>;
+	os: 'windows' | 'linux/macos';
+} = {
+	packageManager: 'npm',
+	os: 'windows',
+};
 
 vi.mock('fs/promises', async () => {
 	return {
@@ -92,7 +138,7 @@ vi.mock('child_process', async () => {
 			setTimeout(() => {
 				event.stdout.emit(
 					'data',
-					currentPackageManagerMock === 'yarn (berry)' ? '3.0.0' : '1.0.0'
+					currentMocks.packageManager === 'yarn (berry)' ? '3.0.0' : '1.0.0'
 				);
 				event.emit('close', 0);
 			}, 100);
