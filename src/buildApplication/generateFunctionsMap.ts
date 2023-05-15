b3ff89fe3fc2d44544329163e4a093564358af7e
Felix Schultz <25091110+felix-schultz@users.noreply.github.com>
2023-05-15 17:39:08 +0200

    Added "node:events" to external config for the functionsmap generation. #244 (#246)
    * Added "node:events" to external config for the functionsmap generation. #244
    
    * Wildcard node modules + Changeset
    
    changed node:buffer, node:events -> node:*
    
    ---------
    
    Co-authored-by: Felix Schultz <felix.schultz@ca9.io>

--- src/buildApplication/generateFunctionsMap.ts
+++ src/buildApplication/generateFunctionsMap.ts
@@ -246,7 +246,7 @@ async function processFuncDirectory(
 		platform: 'neutral',
 		outfile: newFilePath,
 		bundle: true,
-		external: ['node:buffer', `${relativeChunksPath}/*`],
+		external: ['node:*', `${relativeChunksPath}/*`],
 		minify: true,
 		plugins: [nodeBufferPlugin],
 	});
@@ -399,7 +399,7 @@ async function buildWebpackChunkFiles(
 			platform: 'neutral',
 			outfile: chunkFilePath,
 			bundle: true,
-			external: ['node:buffer'],
+			external: ['node:*'],
 			minify: true,
 			plugins: [nodeBufferPlugin],
 		});
