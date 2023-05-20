ed3504e31b87e2df30fe62e249c6b1c52b289328
Dario Piotrowicz <dario@cloudflare.com>
2023-05-20 22:00:34 +0100

    add tsdocs

--- src/utils/getRequestCfProperties.ts
+++ src/utils/getRequestCfProperties.ts
@@ -1,8 +1,14 @@
 import { getCloudflareGlobalContext } from './utils/cloudflareGlobalContext';
 
-export function getRequestCfProperties():
-	| IncomingRequestCfProperties
-	| undefined {
+/**
+ * returns the cf object part of the incoming request
+ *
+ * Note: this function throws when run on the client, where there is no request
+ *       to take the cf object from
+ *
+ * @returns the cf object if it was present in the request, undefined otherwise
+ */
+export function getRequestCfProperties(): IncomingRequestCfProperties|undefined {
 	const cloudflareGlobalContext = getCloudflareGlobalContext();
 	if (!cloudflareGlobalContext) {
 		throw new Error(
--- src/utils/getRequestExecutionContext.ts
+++ src/utils/getRequestExecutionContext.ts
@@ -1,6 +1,13 @@
 import { getCloudflareGlobalContext } from './utils/cloudflareGlobalContext';
 
-export function getRequestExecutionContext(): ExecutionContext | undefined {
+/**
+ * returns the request's execution context (usually referred as ctx).
+ *
+ * Note: this function throws when run on the client, where there is execution context
+ *
+ * @returns the request's execution context
+ */
+export function getRequestExecutionContext(): ExecutionContext {
 	const cloudflareGlobalContext = getCloudflareGlobalContext();
 	if (!cloudflareGlobalContext) {
 		throw new Error(
