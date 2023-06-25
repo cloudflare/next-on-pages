951a9ce9714d05e01a01a4dd5f666b6bd5f49d18
James <james@eli.cx>
2023-06-25 19:08:27 +0100

    this is what going insane looks like

--- packages/next-on-pages/src/buildApplication/buildSummary.ts
+++ packages/next-on-pages/src/buildApplication/buildSummary.ts
@@ -6,71 +6,6 @@ import type { DirectoryProcessingResults } from './generateFunctionsMap';
 import type { ProcessedVercelOutput } from './processVercelOutput';
 
 /**
- * Processes a map of items into a list that can be used in the build summary.
- *
- * Filters out `.rsc` files and strips `.html` from the end of the path, sorting the list
- * alphabetically.
- *
- * @param items A map of items to process.
- * @returns A list of items to be used in the build summary.
- */
-function processItemsMap(items: Map<string, unknown>): string[] {
-	return [...items.keys()]
-		.filter(path => !/\.rsc$/.test(path))
-		.map(path => addLeadingSlash(stripIndexRoute(path.replace(/\.html$/, ''))))
-		.sort((a, b) => a.localeCompare(b));
-}
-
-/**
- * Returns a prefix to be used in the printed build summary for an item's index.
- *
- * @param total Total number of items.
- * @param idx Current item index.
- * @returns The prefix for the log message.
- */
-function getItemPrefix(total: number, idx: number): string {
-	if (total === 1) return '-';
-	if (idx === 0) return '┌';
-	if (idx === total - 1) return '└';
-	return '├';
-}
-
-/**
- * Adds a group of items to the build summary, limiting the number of items to be displayed if
- * necessary.
- *
- * Formats the items with stylised prefixes and indentations.
- *
- * @param summary Current summary.
- * @param name The name of the group of items.
- * @param rawItems List of items to be added to the summary.
- * @param limit Maximum number of items to be displayed.
- * @returns The updated summary.
- */
-function addToSummary(
-	summary: string,
-	name: string,
-	rawItems: string[],
-	limit?: number
-): string {
-	if (rawItems.length === 0) return summary;
-
-	const items =
-		limit && rawItems.length > limit
-			? [
-					...rawItems.slice(0, limit - 1),
-					`... ${rawItems.length - limit - 1} more`,
-			  ]
-			: rawItems;
-
-	const formattedItems = items
-		.map((path, idx) => `  ${getItemPrefix(items.length, idx)} ${path}`)
-		.join('\n');
-
-	return `${summary}\n\n${name} (${rawItems.length})\n${formattedItems}`;
-}
-
-/**
  * Prints a build summary to the console.
  *
  * @param staticAssets List of static assets collected during the build.
@@ -108,12 +43,15 @@ export function printBuildSummary(
 			return a.localeCompare(b);
 		});
 
-	let summary = `Build Summary (@cloudflare/next-on-pages v${nextOnPagesVersion})`;
-	summary = addToSummary(summary, 'Middleware Functions', middlewareFunctions);
-	summary = addToSummary(summary, 'Edge Function Routes', [...routeFunctions]);
-	summary = addToSummary(summary, 'Prerendered Routes', prerendered, 20);
-	summary = addToSummary(summary, 'Wasm Files', [...wasmIdentifiers.keys()]);
-	summary = addToSummary(summary, 'Other Static Assets', otherStatic, 5);
+	const summaryTitle = `Build Summary (@cloudflare/next-on-pages v${nextOnPagesVersion})`;
+	const summarySections = constructSummarySections([
+		{ name: 'Middleware Functions', rawItems: middlewareFunctions },
+		{ name: 'Edge Function Routes', rawItems: [...routeFunctions] },
+		{ name: 'Prerendered Routes', rawItems: prerendered, limit: 20 },
+		{ name: 'Wasm Files', rawItems: [...wasmIdentifiers.keys()] },
+		{ name: 'Other Static Assets', rawItems: otherStatic, limit: 5 },
+	]);
+	const summary = `${summaryTitle}\n\n${summarySections}`;
 
 	cliLog(summary, { spaced: true, skipDedent: true });
 }
@@ -138,7 +76,7 @@ export async function writeBuildInfo(
 	}: Partial<DirectoryProcessingResults> = {}
 ): Promise<void> {
 	const currentDir = resolve();
-	const filePath = join(outputDir, 'nop-build-log.json');
+	const buildLogFilePath = join(outputDir, 'nop-build-log.json');
 
 	// Change wasm paths to be relative to avoid leaking file system structure or account username.
 	const desensitizedWasmIdentifiers = [...wasmIdentifiers.values()].map(
@@ -148,31 +86,94 @@ export async function writeBuildInfo(
 		})
 	);
 
-	await mkdir(outputDir, { recursive: true });
-	await writeFile(
-		filePath,
-		JSON.stringify(
-			{
-				version: nextOnPagesVersion,
-				timestamp: Date.now(),
-				outputDir: relative(currentDir, outputDir),
-				buildFiles: {
-					invalidFunctions: [...invalidFunctions.values()],
-					middlewareFunctions: [...vercelOutput.entries()]
-						.filter(([, { type }]) => type === 'middleware')
-						.map(([path]) => path),
-					edgeFunctions: [...functionsMap.keys()],
-					prerenderFunctionFallbackFiles: [...prerenderedRoutes.keys()],
-					wasmFiles: desensitizedWasmIdentifiers,
-					staticAssets: staticAssets.filter(
-						path => !prerenderedRoutes.has(path)
-					),
-				},
-			},
-			null,
-			2
-		)
-	);
+	const buildLogObject = {
+		timestamp: Date.now(),
+		outputDir: relative(currentDir, outputDir),
+		versions: {
+			'@cloudflare/next-on-pages': nextOnPagesVersion,
+		},
+		buildFiles: {
+			invalidFunctions: [...invalidFunctions.values()],
+			middlewareFunctions: [...vercelOutput.entries()]
+				.filter(([, { type }]) => type === 'middleware')
+				.map(([path]) => path),
+			edgeFunctions: [...functionsMap.keys()],
+			prerenderFunctionFallbackFiles: [...prerenderedRoutes.keys()],
+			wasmFiles: desensitizedWasmIdentifiers,
+			staticAssets: staticAssets.filter(path => !prerenderedRoutes.has(path)),
+		},
+	};
+	const buildLogText = JSON.stringify(buildLogObject, null, 2);
 
-	cliLog(`Build log saved to '${relative(currentDir, filePath)}'`);
+	await mkdir(outputDir, { recursive: true });
+	await writeFile(buildLogFilePath, buildLogText);
+
+	cliLog(`Build log saved to '${relative(currentDir, buildLogFilePath)}'`);
+}
+
+/**
+ * Processes a map of items into a list that can be used in the build summary.
+ *
+ * Filters out `.rsc` files and strips `.html` from the end of the path, sorting the list
+ * alphabetically.
+ *
+ * @param items A map of items to process.
+ * @returns A list of items to be used in the build summary.
+ */
+function processItemsMap(items: Map<string, unknown>): string[] {
+	return [...items.keys()]
+		.filter(path => !/\.rsc$/.test(path))
+		.map(path => addLeadingSlash(stripIndexRoute(path.replace(/\.html$/, ''))))
+		.sort((a, b) => a.localeCompare(b));
+}
+
+/**
+ * Returns a prefix to be used in the printed build summary for an item's index.
+ *
+ * @param total Total number of items.
+ * @param idx Current item index.
+ * @returns The prefix for the log message.
+ */
+function getItemPrefix(total: number, idx: number): string {
+	if (total === 1) return '-';
+	if (idx === 0) return '┌';
+	if (idx === total - 1) return '└';
+	return '├';
+}
+
+/**
+ * Constructs a build summary for a group of sections.
+ *
+ * Formats the items with stylised prefixes and indentations.
+ *
+ * @param sections Build summary sections.
+ * @returns The build summary.
+ */
+function constructSummarySections(
+	sections: {
+		name: string;
+		rawItems: string[];
+		limit?: number;
+	}[]
+): string {
+	return sections
+		.map(({ name, rawItems, limit }) => {
+			if (rawItems.length === 0) return null;
+
+			const items =
+				limit && rawItems.length > limit
+					? [
+							...rawItems.slice(0, limit - 1),
+							`... ${rawItems.length - limit - 1} more`,
+					  ]
+					: rawItems;
+
+			const formattedItems = items
+				.map((path, idx) => `  ${getItemPrefix(items.length, idx)} ${path}`)
+				.join('\n');
+
+			return `${name} (${rawItems.length})\n${formattedItems}`;
+		})
+		.filter(Boolean)
+		.join('\n\n');
 }
