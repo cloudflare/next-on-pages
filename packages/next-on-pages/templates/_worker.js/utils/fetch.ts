146cc2650a51024694e98a33702458e0be14ee91
Dario Piotrowicz <dario@cloudflare.com>
2023-07-12 11:29:30 +0100

    add comment before response creation

--- packages/next-on-pages/templates/_worker.js/utils/fetch.ts
+++ packages/next-on-pages/templates/_worker.js/utils/fetch.ts
@@ -25,11 +25,21 @@ function applyPatch() {
 					await import(`./__next-on-pages-dist__/assets/${url.pathname}.bin`)
 				).default;
 
-				return {
+				// Note: we can't generate a real Response object here because this fetch might be called
+				//       at the top level of a dynamically imported module, and such cases produce the following
+				//       error:
+				//           Some functionality, such as asynchronous I/O, timeouts, and generating random values,
+				//           can only be performed while handling a request
+				//       this is a somewhat known workerd behavior (currently kept for security and performance reasons)
+				//
+				//       if the above issue/constraint were to change we should replace the following with a real Response object
+				const resp = {
 					arrayBuffer() {
 						return binaryContent;
 					},
 				} as Response;
+
+				return resp;
 			} catch {
 				/* empty, let's just fallback to the original fetch */
 			}
