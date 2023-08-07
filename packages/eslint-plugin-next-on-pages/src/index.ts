511b768b2c3f69d22b4d1caa3a57117d94de13c9
Dario Piotrowicz <dario@cloudflare.com>
2023-08-07 12:59:05 +0100

    add no-app-not-found-runtime eslint rule

--- packages/eslint-plugin-next-on-pages/src/index.ts
+++ packages/eslint-plugin-next-on-pages/src/index.ts
@@ -1,5 +1,6 @@
 import noNodeJsRuntime from './rules/no-nodejs-runtime';
 import noUnsupportedConfigs from './rules/no-unsupported-configs';
+import noAppNotFound from './rules/no-app-not-found-runtime';
 
 import type { ESLint } from 'eslint';
 
@@ -7,11 +8,13 @@ const config: ESLint.Plugin = {
 	rules: {
 		'no-nodejs-runtime': noNodeJsRuntime,
 		'no-unsupported-configs': noUnsupportedConfigs,
+		'no-app-not-found': noAppNotFound,
 	},
 	configs: {
 		recommended: {
 			plugins: ['eslint-plugin-next-on-pages'],
 			rules: {
+				'next-on-pages/no-app-not-found': 'error',
 				'next-on-pages/no-nodejs-runtime': 'error',
 				'next-on-pages/no-unsupported-configs': 'error',
 			},
