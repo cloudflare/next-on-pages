b1ed49cd642664da60cd6d1e8661d94a5f2b1914
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2023-11-11 14:11:31 +0000

    Update packages/next-on-pages/src/buildApplication/processVercelFunctions/dedupeEdgeFunctions.ts

--- packages/next-on-pages/src/buildApplication/processVercelFunctions/dedupeEdgeFunctions.ts
+++ packages/next-on-pages/src/buildApplication/processVercelFunctions/dedupeEdgeFunctions.ts
@@ -484,7 +484,7 @@ function fixFunctionContents(contents: string): string {
 		'$1',
 	);
 
-	// TODO: Remove once `vercel/next.js#58265` is fixed.
+	// TODO: Remove once https://github.com/vercel/next.js/issues/58265 is fixed.
 	// This resolves a critical issue in Next.js 14.0.2 that breaks edge runtime rendering due to the assumption
 	// that the the passed internal request is of type `NodeNextRequest` and never `WebNextRequest`.
 	contents = contents.replace(
