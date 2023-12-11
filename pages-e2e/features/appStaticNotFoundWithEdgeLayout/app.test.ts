7969d6cc1c918be92e40583143220c25cc98ca21
Dario Piotrowicz <dario@cloudflare.com>
2023-12-11 10:54:28 +0000

    remove copy-pasted code

--- pages-e2e/features/appStaticNotFoundWithEdgeLayout/app.test.ts
+++ pages-e2e/features/appStaticNotFoundWithEdgeLayout/app.test.ts
@@ -42,19 +42,4 @@ describe('app static not found with edge layout', () => {
 			/\[server side info\] the request's accept header value is: "[^"]*?\*\/\*[^"]*?"/,
 		);
 	});
-
-	// test('/a/random/path should display the catch-all page', async () => {
-	// 	const page = await BROWSER.newPage();
-	// 	const assertVisible = getAssertVisible(page);
-	// 	const pageUrl = `${DEPLOYMENT_URL}/a/random/path`;
-	// 	await page.goto(pageUrl);
-	// 	await assertVisible('h1', { hasText: '[[...path]].tsx Page' });
-	// });
-
-	// test('static assets should be correctly served', async ({ expect }) => {
-	// 	const response = await fetch(`${DEPLOYMENT_URL}/next.svg`);
-	// 	expect(await response.text()).toMatch(
-	// 		/^<svg xmlns="http:\/\/www.w3.org\/2000\/svg" fill="none" viewBox="0 0 394 80">.*<\/svg>/,
-	// 	);
-	// });
 });
