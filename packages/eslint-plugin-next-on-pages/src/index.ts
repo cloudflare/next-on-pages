ed9a4ac7e5303979ab1355b8b3c6ee7914c2f198
Dario Piotrowicz <dario@cloudflare.com>
2023-12-21 21:11:11 +0000

    remove `no-app-not-found-runtime` eslint rule in a backward compatible manner (#600)

--- packages/eslint-plugin-next-on-pages/src/index.ts
+++ packages/eslint-plugin-next-on-pages/src/index.ts
@@ -1,6 +1,5 @@
 import noNodeJsRuntime from './rules/no-nodejs-runtime';
 import noUnsupportedConfigs from './rules/no-unsupported-configs';
-import noAppNotFoundRuntime from './rules/no-app-not-found-runtime';
 
 import type { ESLint } from 'eslint';
 
@@ -8,13 +7,14 @@ const config: ESLint.Plugin = {
 	rules: {
 		'no-nodejs-runtime': noNodeJsRuntime,
 		'no-unsupported-configs': noUnsupportedConfigs,
-		'no-app-not-found-runtime': noAppNotFoundRuntime,
+		// the following rule is no longer needed/applicable, it has been converted into a noop (so that it doesn't introduce a breaking change)
+		// it should be removed in the next package major release
+		'no-app-not-found-runtime': () => ({}),
 	},
 	configs: {
 		recommended: {
 			plugins: ['eslint-plugin-next-on-pages'],
 			rules: {
-				'next-on-pages/no-app-not-found-runtime': 'error',
 				'next-on-pages/no-nodejs-runtime': 'error',
 				'next-on-pages/no-unsupported-configs': 'error',
 			},
