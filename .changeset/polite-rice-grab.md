3dd1f20db849cc69f6add0a86ecd4afc33833691
Dario Piotrowicz <dario@cloudflare.com>
2024-01-18 16:43:18 +0000

    indent `setupDevBindings` warnings (#643)

---
+++ .changeset/polite-rice-grab.md
@@ -0,0 +1,5 @@
+---
+'@cloudflare/next-on-pages': patch
+---
+
+indent `setupDevBindings` warnings
--- internal-packages/next-dev/src/utils.ts
+++ internal-packages/next-dev/src/utils.ts
@@ -3,13 +3,13 @@ export function warnAboutExternalBindingsNotFound(
 	typeOfBinding: 'Durable Objects' | 'Service Bindings',
 ): void {
 	console.warn(
-		`\n\x1b[33mWarning:\nYou have requested ${typeOfBinding} but no local instance of such` +
-			` has been found.\nIn order to access your ${typeOfBinding} please start the relevant workers locally\n` +
-			'with `wrangler dev` and then restart the next dev server.\n\n' +
+		`\n\x1b[33mWarning:\n  You have requested ${typeOfBinding} but no local instance of such` +
+			` has been found.\n  In order to access your ${typeOfBinding} please start the relevant workers locally\n  ` +
+			'with `wrangler dev` and then restart the next dev server.\n\n  ' +
 			`The following bindings won't be accessible until then:\n ${[
 				...bindingsNotFound,
 			]
-				.map(notFound => ` - ${notFound}`)
+				.map(notFound => `   - ${notFound}`)
 				.join('\n')}\x1b[0m\n\n`,
 	);
 }
