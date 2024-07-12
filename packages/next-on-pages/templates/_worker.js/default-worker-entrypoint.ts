f3bfcae2337fc930b61cd0012900656bf0ccaf8d
James <james@eli.cx>
2024-07-12 17:34:14 +0100

    .

--- packages/next-on-pages/templates/_worker.js/default-worker-entrypoint.ts
+++
@@ -1,7 +0,0 @@
-declare const __NEXT_ON_PAGES__: { fetch: ExportedHandlerFetchHandler };
-
-export default {
-	async fetch(request, env, ctx) {
-		return __NEXT_ON_PAGES__.fetch(request, env, ctx);
-	},
-} as ExportedHandler;
