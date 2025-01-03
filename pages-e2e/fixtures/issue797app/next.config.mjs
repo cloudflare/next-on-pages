bbbd81fba294b8f7378e550bb2904b0e73f4e66c
Nora Söderlund <norasoderlund@icloud.com>
2025-01-03 16:25:27 +0100

    Update next.config.mjs

--- pages-e2e/fixtures/issue797app/next.config.mjs
+++ pages-e2e/fixtures/issue797app/next.config.mjs
@@ -4,7 +4,9 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/5712c57ea7/internal-packages/next-dev/README.md
 if (process.env.NODE_ENV === 'development') {
-	await setupDevPlatform();
+  (async () => {
+    await setupDevPlatform();
+  })();
 }
 
 /** @type {import('next').NextConfig} */
